abff2d80f63fce624e271547f497bc351529f364
Noa <noa.oso01@estudiants.upf.edu>
2026-04-10 11:05:08 +0200

    demanar el mapa

--- src/main.c
+++ src/main.c
@@ -1,1 +1,17 @@
+#include "sample_lib.h"
+#include <sys/stat.h>
 
+void createleak(){
+    char *foo = malloc(20 * sizeof(char)); 
+    printf("Allocated leaking string: %s", foo); 
+
+}
+
+int main(){
+char map_name[5]; 
+
+    printf("Enter map name (e.g. 'xs_2' or 'xl_1'):");
+    scanf("%s", map_name);  
+
+    return 0; 
+}
